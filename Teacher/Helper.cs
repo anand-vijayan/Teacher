54a120c57c55073bed29b4c276aed6fcb373e840
Anand Vijayan <anand.vijayan@outlook.com>
2020-09-24 08:12:08 +0530

    Initial Commit

---
+++ Teacher/Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teacher
+{
+    public class Helper
+    {
+        public Dictionary<char, Action> dictionaryObj;
+
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+
+        public Helper(int _val1, int _val2)
+        {
+            dictionaryObj = new Dictionary<char, Action>();
+            PopulateDictionary();
+            value1 = _val1;
+            value2 = _val2;
+        }
+
+        private void PopulateDictionary()
+        {
+            dictionaryObj.Add('+', AddValues);
+            dictionaryObj.Add('-', SubtractValues);
+            dictionaryObj.Add('*', MultiplyValues);
+            dictionaryObj.Add('/', DivideValues);
+        }
+
+        public void AddValues()
+        {
+            Console.WriteLine($"Output: {value1 + value2}");
+        }
+
+        public void SubtractValues()
+        {
+            Console.WriteLine($"Output: {value1 - value2}");
+        }
+
+        public void MultiplyValues()
+        {
+            Console.WriteLine($"Output: {value1 * value2}");
+        }
+
+        public void DivideValues()
+        {
+            Console.WriteLine($"Output: {value1 / value2}");
+        }
+    }
+}
---
+++ Teacher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Teacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1, value2;
+            char operationSelector;
+            
+            if(args == null)
+            {
+                Console.WriteLine("Expecting 3 parameters, intigerValue1 integerValue2 operationSymbol");
+            }
+            else if(args.Length != 3)
+            {
+                Console.WriteLine("Expecting 3 parameters, intigerValue1 integerValue2 operationSymbol");
+            }
+            else
+            {
+                if(!Int32.TryParse(args[0], out value1))
+                {
+                    Console.WriteLine($"{args[0]} must be integer.");
+                }
+
+                if (!Int32.TryParse(args[1], out value2))
+                {
+                    Console.WriteLine($"{args[1]} must be integer.");
+                }
+
+                if(!char.TryParse(args[2], out operationSelector))
+                {
+                    Console.WriteLine($"{args[2]} symbol not have any corresponding operation. It must be + or - or * or /");
+                }
+
+                Helper helperObj = new Helper(value1, value2);
+
+                if (helperObj.dictionaryObj.ContainsKey(operationSelector))
+                {
+                    Action operationToBeExecuted = helperObj.dictionaryObj[operationSelector];
+                    operationToBeExecuted.Invoke();
+                }
+                else
+                {
+                    Console.WriteLine($"{args[2]} symbol not have any corresponding operation. It must be + or - or * or /");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
